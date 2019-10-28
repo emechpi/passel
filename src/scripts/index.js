1853412378e3ebc7d1da4734d588e417e2b5a3dd
emechpi <emechpi@gmail.com>
2019-10-28 23:54:33 +0330

    card added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,4 +3,15 @@ $('document').ready(function() {
         $(this).siblings('.btn').removeClass('active');
         $(this).addClass('active');
     });
+    $('.menu-toggler').on('click', function () {
+        $('body').append('<div class="dark-overlay" onclick="closeMenu()"></div>');
+        $('body').addClass('has-menu');
+        $('.navigation-menu').addClass('show');
+    });
 });
+function closeMenu() {
+    $('body').removeClass('has-menu');
+    $('.dark-overlay').remove();
+    $('.navigation-menu').removeClass('show');
+}
+
