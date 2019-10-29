36ea4e0bd23a9c4dbfb14c5be2000dede13bc3ca
emechpi <emechpi@gmail.com>
2019-10-29 23:41:17 +0330

    add bottom-sheet

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,10 +8,27 @@ $('document').ready(function() {
         $('body').addClass('has-menu');
         $('.navigation-menu').addClass('show');
     });
+    $('.bottom-sheet-toggler').on('click', function () {
+        $('body').append('<div class="blurred-box"></div>');
+        $('body').addClass('has-bottom-sheet');
+        $('.bottom-sheet').addClass('show');
+    });
+    $('.bottom-sheet-closer').on('click', function () {
+        if($('body').hasClass('has-bottom-sheet')) {
+            closeBottomSheet();
+        }else {
+            return false;
+        }
+    });
 });
 function closeMenu() {
     $('body').removeClass('has-menu');
     $('.dark-overlay').remove();
     $('.navigation-menu').removeClass('show');
+}
+function closeBottomSheet() {
+    $('body').removeClass('has-bottom-sheet');
+    $('.blurred-box').remove();
+    $('.bottom-sheet').removeClass('show');
 }
 
